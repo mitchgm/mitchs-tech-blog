c585b4d326df815e963a97428067c35629bdbfb7
mitchgm <mitchgm@bgsu.edu>
2022-10-18 22:22:59 -0700

    homeroute in cont folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,8 +5,37 @@ const sequelize = require("../config/connection");
 // set dependencies
 
 
+// this will render our home page
+router.get("/", (req, res) => {
+    Post.findAll({
+        // this will gather our posts
 
 
+        // this sets our post criteria aka attributes
+        attributes: [
+            "id",
+            "postText",
+            "title",
+
+        ],
+    }
+    )
+
+},
+
+    router.get("/login", (req, res) => {
+        // this allows us to rendow our login page unless they have logged in already
+        if (req.session.loggedIn) {
+            res.redirect("/");
+            return;
+        }
+
+        res.render("login");
+    }));
+
+
+
+module.exports = router;
 
 
 
